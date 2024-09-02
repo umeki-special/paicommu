365c157342a75cbccdfef4bf26afc4d5bbf39ad9
umeki-special <manmosukowai@gmail.com>
2024-09-02 16:26:01 +0900

    0902

--- db/migrate/20240902050030_create_users.rb
+++ db/migrate/20240902050030_create_users.rb
@@ -2,14 +2,13 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :username, null: false
-      t.string :email, null: false, unique: true
+      t.string :email, null: false
       t.string :password_digest, null: false
       t.string :family_name, null: false
       t.string :first_name, null: false
       t.string :family_name_kana, null: false
       t.string :first_name_kana, null: false
       t.date :birth_day, null: false
-
       t.timestamps
     end
 
